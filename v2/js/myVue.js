692ca0af8b19f97321476ad3c5efbd1631af6df9
xiongjuhui <xiongjuhui@linglie.com>
2019-03-19 18:19:30 +0800

    MyVue v.2-doing

--- v2/js/myVue.js
+++ v2/js/myVue.js
@@ -1,56 +1,95 @@
-
-
 //将DOM节点转为文档节点且赋值
 function nodeToFragment(node, vm) {
-	var pp = document.createDocumentFragment(),
-		child = null;
+    var pp = document.createDocumentFragment(),
+        child = null;
 
-	while (child = node.firstChild) {
-		// console.log(child)
-		compile(child, vm);
-		pp.appendChild(child);
-	}
+    while (child = node.firstChild) {
+        // console.log(child)
+        compile(child, vm);
+        pp.appendChild(child);
+    }
 
-	return pp;
+    return pp;
 
 }
 
 //将有vue指令进行编译
 function compile(node, vm) {
-	var reg = /\{\{(.*)\}\}/,
-		attr;
+    var reg = /\{\{(.*)\}\}/,
+        attr;
 
-	//元素节点
-	if (node.nodeType==1) {
-		attr = node.attributes;
+    //元素节点
+    if (node.nodeType == 1) {
+        attr = node.attributes;
 
-		for (var i = 0; i < attr.length; i++) {
-			if (attr[i].nodeName==="v-model") {
-				var val = attr[i].nodeValue;
-				node.value = vm.data[val];
-				node.removeAttribute('v-model')
-			}
-		}
-	}
+        for (var i = 0; i < attr.length; i++) {
+            if (attr[i].nodeName === "v-model") {
+                var val = attr[i].nodeValue;
+                //添加事件
+                node.addEventListener('input', function(e) {
+                    // console.log('e:', e.target.value)
+                    vm[val] = e.target.value;
+                })
 
-	//如果为文本节点
-	if (node.nodeType==3) {
-		if (reg.test(node.nodeValue)) {
-			var name = RegExp.$1;
-			node.nodeValue = vm.data[name];
-		}
-	}
+                node.value = vm.data[val];
+                node.removeAttribute('v-model');
+                
+
+            }
+        }
+    }
+
+    //如果为文本节点
+    if (node.nodeType == 3) {
+        if (reg.test(node.nodeValue)) {
+            var name = RegExp.$1;
+            node.nodeValue = vm.data[name];
+        }
+    }
 }
+
+
+function defineReactive(vm, key, val) {
+    // 需要用一个中间变量存储data中变量的值
+    Object.defineProperty(vm, key, {
+        get: function () {
+            return val;
+        },
+        set: function (newVal) {
+            if (newVal === val) {
+                return; 
+            } else {
+                val = newVal;
+                console.log(val); // 方便看效果
+            }
+        }
+    });
+
+}
+
+function observe(obj, vm) {
+    Object.keys(obj).forEach(key => {
+        defineReactive(vm, key, obj[key]);
+    })
+}
+
 
 function MyVue(options) {
-	// body...
-	this.data = options.data;
+    this.data = options.data;
 
-	var app = document.querySelector(options.el);
+    observe(this.data, this);
 
-	var frag = nodeToFragment(app, this);
+    var app = document.getElementById(options.el);
 
+    var frag = nodeToFragment(app, this);    
 
-	document.querySelector(options.el).appendChild(frag);
+    app.appendChild(frag);
 
 }
+
+// var app = new MyVue({
+// 			el: '#app',
+// 			data: {
+// 				name: 'pitter'
+// 			}
+// 		})
