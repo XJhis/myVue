a65756b59fe1bb747610cc1e8b5da58b1a489afb
xiongjuhui <xiongjuhui@linglie.com>
2019-03-20 11:36:33 +0800

    js发布订阅事件

--- publish/my.js
+++ publish/my.js
@@ -3,19 +3,23 @@ function Publish() {
 }
 
 Publish.prototype = {
-    on: function(type, callback) {
+    on: function(type, fn) {
+        //第一次声明为数组
         if (!this.subs[type]) {
             this.subs[type] = [];
         }
 
-        this.subs[type].push(callback);
+        this.subs[type].push(fn);
     },
     emit: function(type) {
 
+        //将除掉事件名的参数放到一个数组中
         var params = Array.prototype.slice.call(arguments, 1);
 
         if (this.subs[type] && this.subs[type].length) {
             this.subs[type].forEach(item => {
+
+                //将后面的参数都传给item
                 item.apply(this, params)
             })
         } else {
